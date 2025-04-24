6feef59f2f706afc727468f6fc634ee13abff825
Adam <abobich675@gmail.com>
2025-04-23 20:02:11 -0700

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function RootLayout({
   children,
@@ -8,7 +10,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-          {children}
+        <div className='h-15 w-full flex flex-row text-2xl'>
+          <Link href="/" className='flex'>
+            <div className='w-20 p-3 flex items-center hover:bg-gray-200 justify-center'>
+              <Image src="/home.png" alt="hello" width={100} height={100} className='w-full h-full object-contain' />
+            </div>
+          </Link>
+          <Link href="/Resume" className='flex'>
+            <div className='w-20 p-3 flex items-center hover:bg-gray-200 justify-center'>
+              <Image src="/resume.png" alt="hello" width={100} height={100} className='w-full h-full object-contain' />
+            </div>
+          </Link>
+        </div>
+
+        <div className='bg-blue w-full h-1' />
+          
+        {children}
       </body>
     </html>
   )
