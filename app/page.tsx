e749cf17709598c58a3763a01b30cd8ecf7f5e67
Adam <abobich675@gmail.com>
2025-04-24 12:45:08 -0700

    Arth's TypeTextDelete + Updates Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Card from '@/components/Card'
+import TextTypeDelete from '@/components/TextTypeDelete'
  
 export default function Page() {
   return(
   <>
     <div className="h-80 bg-blue-100 text-9xl items-center p-20">
       <div className='pb-5'>
-          hello. i'm adam
+        <TextTypeDelete
+          constText={"Hello! I'm "}
+          typeText={["a programmer.", "a game developer.", "a student.", "Adam."]}
+          fontSize='80px'
+          cursorHeight='80px'
+          cursorWidth='4px'
+          textAlign='left'>
+        </TextTypeDelete>
       </div>
 
       <div className='flex gap-10'>
@@ -23,7 +31,18 @@ export default function Page() {
       </div>
     </div>
 
-    <br />
+    <div className='flex gap-20 p-20'>
+      <div className="text-lg w-full">
+        <div className='text-4xl pb-5'>Hi!</div>
+          I'm Adam, a 2nd year student at Oregon State University studying Computer Science and Artificial Intelligence.
+          I'm excited to implement AI in new ways that push the barriers of what we thought possible.
+          <br/><br/>
+          I also work on coding projects in my free time, such as video game designs, websites, and data visualizations.
+      </div>
+      <div className='flex-shrink-0 justify-end pr-30'>
+        <Image src="/temp.png" alt="Profile Picture" width={200} height={200} />
+      </div>
+    </div>
 
     <div className='flex justify-center'>
       <div className='columns-3'>
@@ -32,14 +51,15 @@ export default function Page() {
         </Link>
 
         <Link href="/Web-Development">
-          <Card icon="/React-Symbol.png" header="Websites" description="X+ Websites developed using Next.js"></Card>
+          <Card icon="/React-Symbol.png" header="Websites" description="15+ Websites developed using React/Next.js"></Card>
         </Link>
 
         <Link href="/Visualizations">
-          <Card icon="/Visualization-Symbol.png" header="Visualizations" description="Data Visualization Designs"></Card>
+          <Card icon="/Visualization-Symbol.png" header="Visualizations" description="Data Visualization and Implementation"></Card>
         </Link>
       </div>
     </div>
+    <br/><br/><br/><br/><br/>
   </>
   )
 }
---
+++ components/TextTypeDelete/TextTypeDelete.tsx
@@ -0,0 +1,160 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import './TextTypeDelete.scss'
+
+
+interface TextTypeDeleteProps {
+	typeText: string[]
+	play?: boolean
+	onComplete?: () => void
+	loop?: boolean
+	blinkGap?: string
+	textAlign?: string
+	constText?: any
+	constTextColor?: string
+	typeTextColor?: string
+	cursorColor?: string
+	pauseMSec?: number
+	typeMSec?: number
+	deleteMSec?: number
+	fontSize?: string
+	cursorHeight?: string
+	cursorWidth?: string
+	constTextStyles?: {}
+	typeTextStyles?: {}
+	style?: {}
+	[x: string]: any
+}
+
+function TextTypeDelete({
+	typeText,
+	play = true,
+	onComplete = () => {},
+	loop = false,
+	textAlign = 'center',
+	blinkGap = '1s',
+	constText = '',
+	constTextColor = 'black',
+	typeTextColor = 'black',
+	cursorColor = 'black',
+	pauseMSec = 1000,
+	typeMSec = 100,
+	deleteMSec = 50,
+	fontSize = '20px',
+	cursorHeight = '22px',
+	cursorWidth = '2px',
+	fontFamily = "'Georgia', 'Courier New', Courier, monospace",
+	constTextStyles = {},
+	typeTextStyles = {},
+	style = {},
+	...rest
+}: TextTypeDeleteProps) {
+	const [text, setText] = useState<string>('')
+	const [tc, setTc] = useState<number[]>([0, -1])
+	const [nextTimeout, setNextTimeout] = useState<number>(pauseMSec)
+	const [lastTimeoutID, setLastTimeoutID] = useState<NodeJS.Timeout>()
+
+	const getTextContent: (arg: any) => string = (elem: any) => {
+		if (!elem) {
+			return ''
+		}
+		if (typeof elem === 'string') {
+			return elem
+		} else if (elem instanceof Array) {
+			let retVal = ''
+
+			elem.forEach(v => {
+				if (typeof v === 'string') retVal += v
+				else retVal += ' ' + getTextContent(v)
+			})
+			return retVal
+		}
+		const children = elem.props?.children
+		if (children instanceof Array) {
+			return children.map(child => getTextContent(child)).join(' ')
+		}
+		return getTextContent(children)
+	}
+
+	const replaceAllDoubleSpaces: (arg: string) => string = (str: string) => {
+		while (str.includes('  ')) str = str.replace('  ', ' ')
+
+		return str
+	}
+
+	useEffect(() => {
+		if (play) {
+			setLastTimeoutID(
+				setTimeout(() => {
+					let temp: number[] = tc[0] == typeText.length ? [0, -1] : tc
+					let deleting: boolean = temp[1] == typeText[temp[0]].length - 1
+
+					if (deleting) {
+						if (!loop && tc[0] == typeText.length - 1) {
+							onComplete()
+							return
+						}
+
+						if (text.length == 1) {
+							if (tc[0] == typeText.length - 1) onComplete()
+							temp = [temp[0] + 1, -1]
+							setNextTimeout(pauseMSec)
+						} else {
+							setNextTimeout(deleteMSec)
+						}
+						setText(text.slice(0, -1))
+					} else {
+						temp = [temp[0], temp[1] + 1]
+						setText(text + typeText[temp[0]][temp[1]])
+						setNextTimeout(temp[1] == typeText[temp[0]].length - 1 ? pauseMSec : typeMSec)
+					}
+
+					setTc(temp)
+				}, nextTimeout),
+			)
+		} else if (lastTimeoutID) {
+			clearTimeout(lastTimeoutID)
+		}
+	}, [text, play])
+
+	return (
+		<div
+			id='ai-text-type-delete-container'
+			style={
+				{
+					'--ai-text-type-delete-cursor-blink': blinkGap,
+					'--ai-text-type-delete-cursor-color': cursorColor,
+					'--ai-text-type-delete-cursor-height': cursorHeight,
+					'--ai-text-type-delete-cursor-width': cursorWidth,
+					'--ai-text-type-delete-font-size': fontSize,
+					'--ai-text-type-delete-font-family': fontFamily,
+					'--ai-text-type-delete-text-align': textAlign,
+					...style,
+				} as React.CSSProperties
+			}
+			aria-label={
+				`An animation with constant text "${replaceAllDoubleSpaces(getTextContent(constText))}" and dynamic text which changes from "${typeText.join('" to "')}".` +
+				(loop ? ' It continually loops through the dynamic text.' : '')
+			}
+			{...rest}
+		>
+			<div
+				id='ai-text-type-delete-text'
+				aria-hidden={true}
+			>
+				<span style={{ color: constTextColor, ...constTextStyles }}>{constText}</span>
+				<span style={{ color: typeTextColor, ...typeTextStyles }}>{text.slice(0, -1)}</span>
+				<span style={{ whiteSpace: 'nowrap' }}>
+					<span style={{ color: typeTextColor, ...typeTextStyles }}>{text.slice(-1)}</span>
+					<div
+						id='ai-text-type-delete-cursor'
+						aria-hidden={true}
+					></div>
+				</span>
+			</div>
+		</div>
+	)
+}
+
+export default TextTypeDelete
