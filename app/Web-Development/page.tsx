2051b6e1df942fcecfd081ba601d7d5e998d426c
Adam <abobich675@gmail.com>
2025-04-25 13:25:21 -0700

    Banners for web dev and visualizations

--- app/Web-Development/page.tsx
+++ app/Web-Development/page.tsx
@@ -3,8 +3,8 @@ import Entry from '@/components/Entry'
 export default function Page() {
   return(
   <>
-    <div className="relative h-70 bg-[url('/temp.png')] bg-cover bg-center text-gray-100 text-9xl flex items-center">
-      <div className="absolute inset-0 bg-gradient-to-r from-black to-black/30 z-10" />
+    <div className="relative h-70 bg-[url('/web-dev.png')] bg-cover bg-center text-gray-100 text-9xl flex items-center">
+      <div className="absolute inset-0 bg-gradient-to-r from-black to-black/50 z-10" />
       <div className="relative z-20 pl-10">
         Web Development
       </div>
