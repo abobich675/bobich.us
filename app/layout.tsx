d635aed5320b64b3606205f013aa4c8ca3e566c8
Adam <abobich675@gmail.com>
2025-04-25 18:20:45 -0700

    fixed nav email

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
               <Image src="/resume.png" alt="Resume" width={100} height={100} className='w-full h-full object-contain' />
             </div>
           </Link>
-          <Link href="/mailto:abobich675@gmail.com" className='flex'>
+          <Link href="mailto:abobich675@gmail.com" className='flex'>
             <div className='w-20 p-3 flex items-center hover:bg-gray-200 justify-center'>
               <Image src="/email.png" alt="email" width={100} height={100} className='w-full h-full object-contain' />
             </div>
