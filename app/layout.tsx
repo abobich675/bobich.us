9d07f86ab4442c0ab7f188893364b2c13be6a5cd
Adam <abobich675@gmail.com>
2025-11-19 19:30:05 -0800

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import DotCanvas from '@/components/DotCanvas';
 import './globals.css'
 import NavBar from '@/components/NavBar';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function RootLayout({
   children,
@@ -17,6 +18,7 @@ export default function RootLayout({
       </head>
       <body>
         <div className='fixed -z-10'>
+          <Analytics />
           <DotCanvas xSpacing={30} ySpacing={30} baseRadius={0.25} maxRadius={1} baseOpacity={0.3} maxOpacity={0.7} color={"#006622"}
           glowColor='40, 20, 80' glowMagnitude={400} mouseRadius={600} lagSpeed={0.035}/>
         </div>
