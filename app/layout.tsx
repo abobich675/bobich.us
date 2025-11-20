2d50fb4bf2626042799f14d5d8a9d1ccc7dc0daf
Adam <abobich675@gmail.com>
2025-11-19 19:43:00 -0800

    fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import DotCanvas from '@/components/DotCanvas';
 import './globals.css'
 import NavBar from '@/components/NavBar';
+import Head from './head';
 
 export default function RootLayout({
   children,
@@ -10,12 +11,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <title>Adam Bobich</title>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
-      </head>
       <body>
+        <Head />
         <div className='fixed -z-10'>
           <DotCanvas xSpacing={30} ySpacing={30} baseRadius={0.25} maxRadius={1} baseOpacity={0.3} maxOpacity={0.7} color={"#006622"}
           glowColor='40, 20, 80' glowMagnitude={400} mouseRadius={600} lagSpeed={0.035}/>
