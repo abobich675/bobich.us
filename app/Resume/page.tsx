5baea40a027e108ecbdff968168053e05ab81f1b
Adam <abobich675@gmail.com>
2025-11-20 23:08:13 -0800

    update hat

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return(
-  <div className='px-10 xl:px-50 pt-10 w-full'>
+  <div className='px-10 xl:px-50 pt-20 w-full'>
     <div className="text-4xl pb-5"> My Resume </div>
     <div className="text-xl">
       My Resume is available for <a href='AdamBobich_Resume.pdf' download className='text-(--accent-color) hover:text-[#8855f0]'><u>download as a pdf</u></a>.
