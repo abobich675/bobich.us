f27031e4b5cc830fe32aa533544199fca124f16d
Adam <abobich675@gmail.com>
2025-11-22 15:55:24 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
         <Head />
         <div className='fixed -z-10'>
           <Analytics />
-          <DotCanvas xSpacing={30} ySpacing={30} baseRadius={0.25} maxRadius={1} baseOpacity={0.3} maxOpacity={0.7} color={"#006622"}
+          <DotCanvas xSpacing={30} ySpacing={30} baseRadius={0.25} maxRadius={1} baseOpacity={0.3} maxOpacity={0.8} color={"#006622"}
           glowColor='40, 20, 80' glowMagnitude={400} mouseRadius={600} lagSpeed={0.035}/>
         </div>
 
